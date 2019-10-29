28cb98eb2654d1e8429e386947878d9d816179b9
cdsleaf <cdsleaf@gmail.com>
2019-10-29 18:29:12 +1300

    refactor: modify css in layout to styled-component style

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,29 +7,37 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import styled from "styled-components"
 import useSiteMetadata from "../hooks/use-sitemetadata"
 import Header from "./header"
 import Footer from "./footer"
 import "./layout.css"
 
+const Container = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`;
+
+const MainDiv = styled.main`
+  margin: 0 auto;
+  max-Width: 960;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+  flex-grow: 1;
+`;
+
 const Layout = ({ children }) => {
   const { title, author } = useSiteMetadata().siteMetadata;
 
   return (
-    <>
+    <Container >
       <Header siteTitle={title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-      </div>
+      <MainDiv>
+        {children}
+      </MainDiv>
       <Footer author={author} />
-    </>
+    </Container>
   )
 }
 
