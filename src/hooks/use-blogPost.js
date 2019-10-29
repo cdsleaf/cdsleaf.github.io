a16162a1e6b58c3db00ca599937c347d4c8f9bb1
cdsleaf <cdsleaf@gmail.com>
2019-10-29 23:53:54 +1300

    feat: add hook (useBlogPost) & into index.js

---
+++ src/hooks/use-blogPost.js
@@ -0,0 +1,24 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+const useBlogPost = () => useStaticQuery(graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`)
+
+export default useBlogPost
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,32 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import useBlogPost from "../hooks/use-blogPost"
 //IndexPage
-export default ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi, I am Theo Cho. I am a web developer.</h1>
-    <p>Welcome to my site.</p>
-    <p>Let's build something great.</p>
-    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-    {data.allMarkdownRemark.edges.map(({ node }) => (
-      <div key={node.id}>
-        <Link to={node.fields.slug}>
-          <h3>
-            {node.frontmatter.title}{" "}
-            <span>
-              — {node.frontmatter.date}
-            </span>
-          </h3>
-          <p>{node.excerpt}</p>
-        </Link>
-      </div>
-    ))}
-  </Layout>
-)
-export const query = graphql`
-  query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`
+export default () => {
+  const { allMarkdownRemark } = useBlogPost();
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi, I am Theo Cho. I am a web developer.</h1>
+      <p>Welcome to my site.</p>
+      <p>Let's build something great.</p>
+      <h4>{allMarkdownRemark.totalCount} Posts</h4>
+      {allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link to={node.fields.slug}>
+            <h3>
+              {node.frontmatter.title}{" "}
+              <span>
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </Link>
+        </div>
+      ))}
+    </Layout>
+  )
+}
